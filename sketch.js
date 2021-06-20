1969029f5bcb5b78749f86966f083c152b99c482
Evan Gresham <36225427+Code-Bullet@users.noreply.github.com>
2021-06-20 13:12:38 +1000

    testing some shit

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
 let canvas;
-function setup() {
-    canvas = createCanvas(1000,1000);
-    canvas.parent("canvas")
-}
-
-function draw(){
-    background(100);
-    fill(255);
-    ellipse(500,frameCount%100 *10, 10);
-
-}
+// function setup() {
+//     canvas = createCanvas(1000,1000);
+//     canvas.parent("canvas")
+// }
+//
+// function draw(){
+//     background(100);
+//     fill(255);
+//     ellipse(500,frameCount%100 *10, 10);
+//
+// }
